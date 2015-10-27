6ec80586e63e10bb2f87e97aa4b1621b7ec82012
Rodrigo Ibarra <lookatthemoon@live.com>
2015-10-27 01:57:38 -0600

    Pull refresh ON

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-angular.module('ionicApp', ['ionic', 'checklist-model'])
+angular.module('ionicApp', ['ionic', 'checklist-model', 'ngCordova'])
 
 .config(function ($stateProvider, $urlRouterProvider) {
 
@@ -51,36 +51,14 @@ angular.module('ionicApp', ['ionic', 'checklist-model'])
 
 })
 
-.controller('HomeTabCtrl', function ($scope, $rootScope, consumirAPI) {
-    console.log('HomeTabCtrl');
+.controller('AppCtrl', function ($scope, $cordovaToast, $rootScope, $state, consumirAPI) {
+
     var token = $rootScope.token;
-    var existePueblaMexico = false;
-    var existeMexicoPuebla = false;
-    var opcionPueblaId = 0;
-    var opcionMexicoId = 0;
 
     $scope.sinSolicitudes = 'No hay solicitudes todavía';
     $scope.solicitudesPendientes = {};
     $scope.solicitudesEspera = {};
     $scope.solicitudesConfirmadas = {};
-
-    $scope.weekDayLabels = [
-        'Lu',
-        'Ma',
-        'Mi',
-        'Ju',
-        'Vi',
-        'Sa',
-        'Do'
-    ];
-
-    $scope.puebla = {
-        weekDays: []
-    };
-
-    $scope.mexico = {
-        weekDays: []
-    };
 
     $scope.ofertarSolicitud = function (id) {
         consumirAPI.ofertarSolicitud(id, token, function () {
@@ -96,7 +74,7 @@ angular.module('ionicApp', ['ionic', 'checklist-model'])
 
     recargarSolicitudes();
 
-    function recargarSolicitudes() {
+    function recargarSolicitudes(callback) {
         consumirAPI.obtenerSolicitudes(token, function (solicitudes) {
             if (solicitudes.length > 0) {
                 $scope.sinSolicitudes = '';
@@ -126,8 +104,42 @@ angular.module('ionicApp', ['ionic', 'checklist-model'])
             $scope.solicitudesPendientes = solicitudesPendientes;
             $scope.solicitudesEspera = solicitudesEspera;
             $scope.solicitudesConfirmadas = solicitudesConfirmadas;
+            callback();
         });
     }
+
+    $scope.doRefresh = function () {
+        recargarSolicitudes(function () {
+            $scope.$broadcast('scroll.refreshComplete');
+        });
+    }
+})
+
+.controller('HomeTabCtrl', function ($scope, $rootScope, consumirAPI) {
+    console.log('HomeTabCtrl');
+    var token = $rootScope.token;
+    var existePueblaMexico = false;
+    var existeMexicoPuebla = false;
+    var opcionPueblaId = 0;
+    var opcionMexicoId = 0;
+
+    $scope.weekDayLabels = [
+        'Lu',
+        'Ma',
+        'Mi',
+        'Ju',
+        'Vi',
+        'Sa',
+        'Do'
+    ];
+
+    $scope.puebla = {
+        weekDays: []
+    };
+
+    $scope.mexico = {
+        weekDays: []
+    };
 
     consumirAPI.obtenerOpcionesViaje(token, function (opcionesViaje) {
         //Recorrer todas las opciones
@@ -242,8 +254,8 @@ angular.module('ionicApp', ['ionic', 'checklist-model'])
             grant_type: "password",
             client_id: "1_3bcbxd9e24g0gk4swg0kwgcwg4o8k8g4g888kwc44gcc0gwwk4",
             client_secret: "4ok2x70rlfokc8g0wws8c8kwcokw80k44sg48goc0ok4w0so0k",
-            username: email,
-            password: pass
+            username: 'p@p.com',
+            password: '12345678'
         }).then(function (response) {
 
                 if (!response.data.access_token) {
