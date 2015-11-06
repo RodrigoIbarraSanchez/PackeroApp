015b9b781f75c94080f11ba5434af8f60b3dfe8a
Rodrigo Ibarra <lookatthemoon@live.com>
2015-11-06 01:53:35 -0600

    añadiendo mapa

--- www/js/app.js
+++ www/js/app.js
@@ -33,6 +33,11 @@ angular.module('ionicApp', ['ionic', 'checklist-model', 'ngCordova', "firebase"]
                     templateUrl: 'templates/viajes.html'
                 }
             }
+        })
+        .state('mapa', {
+            url: '/mapa',
+            templateUrl: 'templates/mapa.html',
+            controller: 'MapCtrl'
         });
 
 
@@ -195,6 +200,72 @@ angular.module('ionicApp', ['ionic', 'checklist-model', 'ngCordova', "firebase"]
         });
     }
 })
+
+.controller('MapCtrl', function ($scope, $ionicLoading, $state, $compile) {
+
+    initialize();
+
+    function initialize() {
+        var myLatlng = new google.maps.LatLng(43.07493, -89.381388);
+
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map"),
+            mapOptions);
+
+        //Marker + infowindow + angularjs compiled ng-click
+        var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
+        var compiled = $compile(contentString)($scope);
+
+        var infowindow = new google.maps.InfoWindow({
+            content: compiled[0]
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            title: 'Uluru (Ayers Rock)'
+        });
+
+        google.maps.event.addListener(marker, 'click', function () {
+            infowindow.open(map, marker);
+        });
+
+        $scope.map = map;
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+    $scope.centerOnMe = function () {
+        if (!$scope.map) {
+            alert('No hay mapa');
+        }
+
+        /*$scope.loading = $ionicLoading.show({
+            content: 'Getting current location...',
+            showBackdrop: false
+        });*/
+
+        navigator.geolocation.getCurrentPosition(function (pos) {
+            $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+            //$scope.loading.hide();
+        }, function (error) {
+            alert('Unable to get location: ' + error.message);
+        });
+    };
+
+    $scope.clickTest = function () {
+        alert('Example of infowindow with ng-click');
+    };
+
+    $scope.misEnvios = function () {
+        $state.go('tabs.envios');
+    };
+
+})
+
 
 .controller('HomeTabCtrl', function ($scope, $rootScope, consumirAPI) {
     console.log('HomeTabCtrl');
